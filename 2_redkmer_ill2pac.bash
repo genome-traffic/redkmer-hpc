307a084f4463475a4bbd5ef77cf4646a199e62ad
filicado <nikolai.windbichler@gmail.com>
2016-12-07 11:20:51 +0000

    step 2

--- 2_redkmer_ill2pac.bash
+++ 2_redkmer_ill2pac.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
 #PBS -N redkmer2
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=24:mem=32gb:tmpspace=200gb
+#PBS -l select=1:ncpus=24:mem=32gb:tmpspace=100gb
 
 source $PBS_O_WORKDIR/redkmer.cfg
-module load samtools
-module load bowtie/1.1.1
-module load bowtie
 
-rm $CWD/pacBio_illmapping/mapping_rawdata/*uniq
 ALLMJOBS=()
 ALLFJOBS=()
 
@@ -23,7 +19,7 @@ READEND=$READNUNIT
 for i in $(eval echo "{1..$NODES}")
 
 	do
-   	echo "Doing something in loop $i (out of $NODES) from read $READSTART to read $READEND !"
+   	echo "Align chunk $i (out of $NODES) from read $READSTART to read $READEND !"
 	
 	ACTUALSTART=$(sed -n "$READSTART"p ${pacDIR}/pacMsplitter)
 	ACTUALEND=$(sed -n "$READEND"p ${pacDIR}/pacMsplitter)
@@ -43,53 +39,78 @@ for i in $(eval echo "{1..$NODES}")
 	sed -n "$ACTUALSTART,$(($ACTUALEND-1))"p $pacM > ${pacDIR}/${i}_m_pac.fasta
 
 
-cat > ${CWD}/qsubscripts/malepacbins${i}.bashX <<EOF
+cat > ${CWD}/qsubscripts/malepacbins${i}.bash <<EOF
 #!/bin/bash
 #PBS -N redkmer_mworker
-#PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=24:mem=32gb:tmpspace=700gb
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=24:mem=128gb:tmpspace=850gb
 
 module load bowtie/1.1.1
+#module load bowtie
+
 	echo "==================================== Indexing male chunk ${i} ======================================="
 		cp ${pacDIR}/${i}_m_pac.fasta XXXXX
 		$BOWTIEB XXXXX/${i}_m_pac.fasta XXXXX/${i}_m_pac
 		#$BOWITE2B XXXXX/${i}_m_pac.fasta XXXXX/${i}_m_pac
-		#cp XXXXX/*ebwt $CWD/pacBio_illmapping/index/  2>/dev/null || :
-		#cp XXXXX/*ebwtl $CWD/pacBio_illmapping/index/  2>/dev/null || :
+		
 	echo "==================================== Working on male pacbins chunk ${i} ======================================="
 		cp $illM XXXXX
 		$BOWTIE -a -t -p $CORES -v 0 XXXXX/${i}_m_pac --suppress 1,2,4,5,6,7,8,9 XXXXX/m.fastq 1> XXXXX/male.txt 2> $CWD/pacBio_illmapping/logs/${i}_male_log.txt
+		#BOWTIE2 -x XXXXX/${i}_m_pac -a --no-hd --no-sq --no-unal XXXXX/m.fastq | cut -f3 -d$'\t' > XXXXX/male.txt
 		rm XXXXX/m.fastq
 	echo "==================================== Done male pacbins, sorting for chunck ${i} ===================================="
-		sort -k1b,1 -T XXXXX XXXXX/male.txt -o $CWD/pacBio_illmapping/mapping_rawdata/${i}_male_uniq
+		#sort -k1b,1 -T XXXXX XXXXX/male.txt | uniq -c > $CWD/pacBio_illmapping/mapping_rawdata/${i}_male_uniq
+
+		split --number=l/$CORES XXXXX/male.txt XXXXX/_sorttmp;
+		rm XXXXX/male.txt
+		ls -1 XXXXX/_sorttmp* | (while read SORTFILE; do sort -k1b,1 -T XXXXX/temp YYYYY -o YYYYY & done;
+		wait
+		)
+		sort -m XXXXX/_sorttmp* | uniq -c > XXXXX/${i}_male_uniq
+		cp XXXXX/${i}_male_uniq $CWD/pacBio_illmapping/mapping_rawdata/
+
 	echo "==================================== Done sorting chunk ${i} ! ===================================="
 EOF
 
-	sed 's/XXXXX/$TMPDIR/g' ${CWD}/qsubscripts/malepacbins${i}.bashX > ${CWD}/qsubscripts/malepacbins${i}.bash
+	sed 's/XXXXX/$TMPDIR/g' ${CWD}/qsubscripts/malepacbins${i}.bash > ${CWD}/qsubscripts/malepacbins${i}.bashX
+	sed 's/YYYYY/$SORTFILE/g' ${CWD}/qsubscripts/malepacbins${i}.bashX > ${CWD}/qsubscripts/malepacbins${i}.bash
 
-cat > ${CWD}/qsubscripts/femalepacbins${i}.bashX <<EOF
+
+cat > ${CWD}/qsubscripts/femalepacbins${i}.bash <<EOF
 #!/bin/bash
 #PBS -N redkmer_fworker
-#PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=24:mem=32gb:tmpspace=700gb
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=24:mem=128gb:tmpspace=850gb
 
 module load bowtie/1.1.1
+#module load bowtie
+
 	echo "==================================== Indexing female chunk ${i} ======================================="
 		cp ${pacDIR}/${i}_m_pac.fasta XXXXX
 		$BOWTIEB XXXXX/${i}_m_pac.fasta XXXXX/${i}_m_pac
 		#$BOWITE2B XXXXX/${i}_m_pac.fasta XXXXX/${i}_m_pac
-		#cp XXXXX/*ebwt $CWD/pacBio_illmapping/index/  2>/dev/null || :
-		#cp XXXXX/*ebwtl $CWD/pacBio_illmapping/index/  2>/dev/null || :
+		
 	echo "==================================== Working on female pacbins ======================================="
 		cp $illF XXXXX
 		$BOWTIE -a -t -p $CORES -v 0 XXXXX/${i}_m_pac --suppress 1,2,4,5,6,7,8,9 XXXXX/f.fastq 1> XXXXX/female.txt 2> $CWD/pacBio_illmapping/logs/${i}_female_log.txt
+		#BOWTIE2 -x XXXXX/${i}_m_pac -a --no-hd --no-sq --no-unal XXXXX/f.fastq | cut -f3 -d$'\t' > XXXXX/female.txt
 		rm XXXXX/f.fastq
 	echo "==================================== Done female pacbins, sorting ===================================="
-		sort -k1b,1 -T XXXXX XXXXX/female.txt -o $CWD/pacBio_illmapping/mapping_rawdata/${i}_female_uniq
+		#sort -k1b,1 -T XXXXX XXXXX/female.txt | uniq -c > $CWD/pacBio_illmapping/mapping_rawdata/${i}_female_uniq
+
+		split --number=l/$CORES XXXXX/female.txt XXXXX/_sorttmp;
+		rm XXXXX/female.txt
+		ls -1 XXXXX/_sorttmp* | (while read SORTFILE; do sort -k1b,1 -T XXXXX/temp YYYYY -o YYYYY & done;
+		wait
+		)
+		sort -m XXXXX/_sorttmp* | uniq -c > XXXXX/${i}_female_uniq
+		cp XXXXX/${i}_female_uniq $CWD/pacBio_illmapping/mapping_rawdata/
+
 	echo "==================================== Done sorting ! ===================================="
 EOF
 
-	sed 's/XXXXX/$TMPDIR/g' ${CWD}/qsubscripts/femalepacbins${i}.bashX > ${CWD}/qsubscripts/femalepacbins${i}.bash
+	sed 's/XXXXX/$TMPDIR/g' ${CWD}/qsubscripts/femalepacbins${i}.bash > ${CWD}/qsubscripts/femalepacbins${i}.bashX
+	sed 's/YYYYY/$SORTFILE/g' ${CWD}/qsubscripts/femalepacbins${i}.bashX > ${CWD}/qsubscripts/femalepacbins${i}.bash
 
 	ALLMJOBS[${i}]=$(qsub ${CWD}/qsubscripts/malepacbins${i}.bash)
 	ALLFJOBS[${i}]=$(qsub ${CWD}/qsubscripts/femalepacbins${i}.bash)
@@ -122,7 +143,6 @@ JOBSR=false
 		fi
 	done
 done;
-
 
 echo "==================================== Done step 2! ======================================="
 		
