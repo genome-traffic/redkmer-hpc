deaa06ee0e2980b8b5e7d8716491b40199668dc9
ppapathanos <p.papathanos@gmail.com>
2017-01-15 18:10:36 +0000

    fix sloppy

--- 9_redkmer_kmers_output.bash
+++ 9_redkmer_kmers_output.bash
@@ -42,7 +42,7 @@ awk 'BEGIN {print "kmer_id\tseq\tfemale\tmale\tCQ\tsum\thits_X\thits_A\thits_Y\t
 awk -v OFS="\t" '$1=$1' $TMPDIR/tmpfile_2 > $CWD/kmers/kmer_results2.txt
 
 awk 'BEGIN {print "kmer_id\tseq\tfemale\tmale\tCQ\tsum\thits_X\thits_A\thits_Y\thits_GA\thits_sum\tperchitsX\thits_threshold\tsum_offtargets\tofftargets\tdegen_targets\tcandidate\tlog10sum\tlabel\tselection"} {print}' $CWD/kmers/candidateXkmers.txt > $TMPDIR/tmpfile_1
-awk -v OFS="\t" '$1=$1' $TMPDIR/tmpfile_1 > $CWD/kmers/candidateXkmers.txt
+awk -v OFS="\t" '$1=$1' $TMPDIR/tmpfile_2 > $CWD/kmers/candidateXkmers.txt
 
 
 #make reduced files for plotting
